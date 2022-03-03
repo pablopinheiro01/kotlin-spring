0ef12b052cf587d467108fb5f6ae932f9715de2a
Pablo Pinheiro <pablo-pinheiro@hotmail.com>
2022-03-03 20:53:29 -0300

    Classes de dominio da aplicação

---
+++ src/main/kotlin/br/com/alura/forum/model/Resposta.kt
@@ -0,0 +1,5 @@
+package br.com.alura.forum.model
+
+import java.time.LocalDateTime
+
+data class Resposta( val id: Long? = null, val mensagem : String, val dataCriaca: LocalDateTime = LocalDateTime.now(), val autor: Usuario, val topico: Topico, val solucao: Boolean)
