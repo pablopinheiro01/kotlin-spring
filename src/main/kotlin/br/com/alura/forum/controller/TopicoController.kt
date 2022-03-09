08f1985941bfacd6d6e0acde9fb3ac844ea00e28
Pablo Pinheiro <pablo-pinheiro@hotmail.com>
2022-03-08 21:17:53 -0300

    Alterado os inputs para o padrao Form e os outputs para o padrao View

--- src/main/kotlin/br/com/alura/forum/controller/TopicoController.kt
+++ src/main/kotlin/br/com/alura/forum/controller/TopicoController.kt
@@ -1,7 +1,7 @@
 package br.com.alura.forum.controller
 
-import br.com.alura.forum.dto.TopicoDto
-import br.com.alura.forum.model.Topico
+import br.com.alura.forum.dto.TopicoForm
+import br.com.alura.forum.dto.TopicoView
 import br.com.alura.forum.service.TopicoService
 import org.springframework.web.bind.annotation.*
 
@@ -10,14 +10,13 @@ import org.springframework.web.bind.annotation.*
 class TopicoController(private val topicoService: TopicoService){
 
     @GetMapping
-    fun listar(): List<Topico> = topicoService.listar()
-
+    fun listar(): List<TopicoView> = topicoService.listar()
 
     @GetMapping("/{id}")
-    fun buscaPorId(@PathVariable id:Long): Topico = topicoService.buscaPorId(id)
+    fun buscaPorId(@PathVariable id:Long): TopicoView = topicoService.buscaPorId(id)
 
     @PostMapping
-    fun cadastrar(@RequestBody dto: TopicoDto){
+    fun cadastrar(@RequestBody dto: TopicoForm){
         topicoService.cadastrar(dto)
     }
 
