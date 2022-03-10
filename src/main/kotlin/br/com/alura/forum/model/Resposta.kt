832518fe8c784630ed5ec915ea6372f61676cf91
Pablo Pinheiro <pablo-pinheiro@hotmail.com>
2022-03-10 19:47:13 -0300

    Mapeamento do JPA nas nossas entidades

--- src/main/kotlin/br/com/alura/forum/model/Resposta.kt
+++ src/main/kotlin/br/com/alura/forum/model/Resposta.kt
@@ -1,5 +1,17 @@
 package br.com.alura.forum.model
 
 import java.time.LocalDateTime
+import javax.persistence.*
 
-data class Resposta( val id: Long? = null, val mensagem : String, val dataCriaca: LocalDateTime = LocalDateTime.now(), val autor: Usuario, val topico: Topico, val solucao: Boolean)
+@Entity
+data class Resposta(
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Long? = null,
+    val mensagem : String,
+    val dataCriaca: LocalDateTime = LocalDateTime.now(),
+    @ManyToOne
+    val autor: Usuario,
+    @ManyToOne//muitos para um, no caso da classe topico mapeamos OneToMany=topico trazendo a ref para essa classe
+    val topico: Topico,
+    val solucao: Boolean
+    )
