bc9e7481d66472d793aa7ebbd5f3045dbb81a63d
Pablo Pinheiro <pablo-pinheiro@hotmail.com>
2022-03-09 21:07:04 -0300

    Realizado tratamento para erros genericos do servidor

--- src/main/kotlin/br/com/alura/forum/exception/ExceptionHandler.kt
+++ src/main/kotlin/br/com/alura/forum/exception/ExceptionHandler.kt
@@ -21,4 +21,15 @@ class ExceptionHandler {
             path = request.servletPath
         )
     }
+
+    @ExceptionHandler(Exception::class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    fun handleServerError(exception: Exception, request: HttpServletRequest): ErrorView {
+        return ErrorView(
+            status = HttpStatus.INTERNAL_SERVER_ERROR.value(),
+            error = HttpStatus.INTERNAL_SERVER_ERROR.name,
+            message = exception.message,
+            path = request.servletPath
+        )
+    }
 }
