d9029ecc724483a911d7707ae37e1a015ffca797
Pablo Pinheiro <pablo-pinheiro@hotmail.com>
2022-03-09 21:14:22 -0300

    Adicionado tratamento de erro personalizado de acordo com validação de cada campo do json

--- src/main/kotlin/br/com/alura/forum/dto/TopicoForm.kt
+++ src/main/kotlin/br/com/alura/forum/dto/TopicoForm.kt
@@ -5,8 +5,11 @@ import javax.validation.constraints.NotNull
 import javax.validation.constraints.Size
 
 data class TopicoForm(
-    @field:NotEmpty @field:Size(min = 5, max = 100) val titulo: String,
-    @field:NotEmpty val mensagem: String,
+    @field:NotEmpty(message = "Titulo não pode ser em branco")
+    @field:Size(min = 5, max = 100, message = "Titulo deve ter entre 5 e 100 caracteres")
+    val titulo: String,
+    @field:NotEmpty(message = "Mensagem não pode ser em branco")
+    val mensagem: String,
     @field:NotNull val idCurso: Long,
     @field:NotNull val idUsuario: Long
 )
