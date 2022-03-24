5d7f8c2d65d48f3e673f55c5f6c0c507be756705
Pablo Pinheiro <pablo-pinheiro@hotmail.com>
2022-03-23 21:10:42 -0300

    Alterado as configuracoes iniciais para suportar autenticacao com jwt inserindo os filtros que devem ser executados para gerar JWT e autenticar o usuario

--- src/main/kotlin/br/com/alura/forum/config/SecurityConfiguration.kt
+++ src/main/kotlin/br/com/alura/forum/config/SecurityConfiguration.kt
@@ -1,7 +1,10 @@
 package br.com.alura.forum.config
 
+import br.com.alura.forum.security.JWTAuthenticationFilter
+import br.com.alura.forum.security.JWTLoginFilter
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.http.HttpMethod
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
@@ -9,27 +12,28 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.config.http.SessionCreationPolicy
 import org.springframework.security.core.userdetails.UserDetailsService
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
 
 @Configuration
 @EnableWebSecurity
 class SecurityConfiguration(
-    private val userDetailService: UserDetailsService
+    private val userDetailService: UserDetailsService,
+    private val jwtUtil: JWTUtil
 ) : WebSecurityConfigurerAdapter() {
 
     override fun configure(http: HttpSecurity?) {
         //autoriza qualquer requisição se tiver autenticada
-        http?.authorizeHttpRequests()?.
-        antMatchers("/topicos")?.//mach para a url
-        hasAuthority("LEITURA_ESCRITA")?. //tipo de role que possui permissao
+        http?.
+        csrf()?.disable()?.//desabilitando CSRF para permitir as requests (inseguro para ambientes produtivos)
+        authorizeHttpRequests()?.
+        antMatchers(HttpMethod.POST,"/login")?.permitAll()?.//permitindo todos acessarem o login
         anyRequest()?.
         authenticated()?.
-        and()?.
-        sessionManagement()?.
-        sessionCreationPolicy(SessionCreationPolicy.STATELESS)?.//nao guardo estado
-        and()?.
-        formLogin()?.
-        disable()?.
-        httpBasic()
+        and()
+        http?.addFilterBefore(JWTLoginFilter(authManager = authenticationManager(), jwtUtil = jwtUtil), UsernamePasswordAuthenticationFilter().javaClass)
+        http?.addFilterBefore(JWTAuthenticationFilter(jwtUtil = jwtUtil), UsernamePasswordAuthenticationFilter().javaClass)
+        http?.sessionManagement()?.
+        sessionCreationPolicy(SessionCreationPolicy.STATELESS)//nao guardo estado
     }
 
     @Bean
